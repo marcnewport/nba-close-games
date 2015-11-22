f18880f200cbbfc8a0081a7dcbc7d44eaba1d1b6
Marc Newport <marcnewport@gmail.com>
2015-11-22 14:00:00 +1100

    attempted icon toggle, but abandoned. need to check if execute script has return value

--- js/background.js
+++ js/background.js
@@ -1,7 +1,25 @@
+var enabled = false;
 //listen for extension button click
 chrome.browserAction.onClicked.addListener(function(tab) {
-  //load jquery, then load custom script
-  chrome.tabs.executeScript(null, { file:"js/jquery.min.js" }, function() {
-    chrome.tabs.executeScript(null, { file:"js/manipulate.js" });
+  //only run on watch.nba.com
+  chrome.tabs.query({ 'active':true }, function (tabs) {
+    var url = tabs[0].url;
+
+    if (url.search('watch.nba') > -1) {
+      chrome.tabs.insertCSS(null, { file:"css/manipulate.css" });
+      //load jquery, then load custom script
+      chrome.tabs.executeScript(null, { file:"js/jquery.min.js" }, function() {
+        chrome.tabs.executeScript(null, { file:"js/manipulate.js" });
+      });
+
+      // if (enabled) {
+      //   chrome.browserAction.setIcon({ path:"icons/icon-disabled.png" });
+      //   enabled = false;
+      // }
+      // else {
+      //   chrome.browserAction.setIcon({ path:"icons/icon.png" });
+      //   enabled = true;
+      // }
+    }
   });
 });
