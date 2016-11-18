457e17715d3569f24cb3d4f03c197b533a8eb7c6
Marc Newport <marcnewport@gmail.com>
2016-11-18 19:04:01 +1100

    updated app to run with new site. removed jquery. added overtime check

--- js/manipulate.js
+++ js/manipulate.js
@@ -1,39 +1,37 @@
-$('.gameItem').each(function() {
-  //should prolly do this with vanilla js...
-  var $this = $(this),
-      $insert = $this.find('.ncge-insert'),
-      awayScore = Number($this.find('.awayscore').text()),
-      homeScore = Number($this.find('.homescore').text()),
-      pointsDiff = 0,
-      closeDiff = 3,
-      closeGame = false;
+document.querySelectorAll('.schedule-item').forEach(function(itemEl) {
 
-  //toggle inserted text
-  if ($insert.length) {
-    //remove any previously inserted text
-    $insert.remove();
-  }
-  //are there points on the board?
-  else if (awayScore || homeScore) {
-    //figure out points difference
-    if (awayScore === homeScore) {
-      closeGame = true;
-    }
-    else if (awayScore > homeScore) {
-      pointsDiff = awayScore - homeScore;
-    }
-    else {
-      pointsDiff = homeScore - awayScore;
-    }
+    // Check if the game went into overtime
+    var gameEl = itemEl.querySelector('.game-situation');
+    var overtime = gameEl.innerHTML === 'Final OT' ? true : false;
 
-    if (pointsDiff <= closeDiff) {
-      closeGame = true;
-    }
+    // Retrieve the scores from the DOM
+    var scores = itemEl.querySelectorAll('.team-score:not(.hide-score)');
+    var awayScore = Number(scores[0].innerHTML);
+    var homeScore = Number(scores[1].innerHTML);
 
-    if (closeGame) {
-      $this.find('.normal').append(
-        $('<span>').addClass('ncge-insert').html('CLOSE GAME')
-      );
+    // Calculate the difference
+    var pointsDiff = Math.abs(awayScore - homeScore);
+
+
+    // TODO get game data and highlight other interesting points about the game
+    // var gameId = '';
+    // var gameData = 'http://stats.nba.com/stats/boxscoresummaryv2/?GameID=0021600133';
+
+
+    // Get the elements we want to manipulate
+    var gameInfoEl = itemEl.querySelector('.game-info');
+    var closeGameEl = gameInfoEl.querySelectorAll('.ncge-insert');
+
+    // Remove our inserted element if it's already there
+    if (closeGameEl.length) {
+        gameInfoEl.removeChild(closeGameEl.item(0));
     }
-  }
+    else if (overtime || pointsDiff < 3) {
+        // Create and insert our new element
+        closeGameEl = document.createElement('div');
+        closeGameEl.className = 'ncge-insert';
+        closeGameEl.innerHTML = 'Close game';
+
+        gameInfoEl.appendChild(closeGameEl);
+    }
 });
